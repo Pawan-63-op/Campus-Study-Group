67cd975f4c105076945ce31db87d54b8fee40dcd
MokshgnaIsm <gorantlamokshgna980@gmail.com>
2026-03-29 20:22:51 +0530

    reg / login / identify functionality added

---
+++ backend/dbUtils/mongoConnect.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+
+export async function connectToDatabase() {
+    const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/testDB';
+    try {
+        await mongoose.connect(mongoURI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('Connected to MongoDB successfully');
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error);
+        throw error;
+    }
+}
---
+++ backend/middleware/identify.js
@@ -0,0 +1,25 @@
+import { sql } from "../dbUtils/sql_utl/sql_connector.js";
+import jwt from 'jsonwebtoken';
+export async function identify(req,res,next) {
+    const authHeader = req.headers['authorization'];
+    if (!authHeader) {
+        return res.status(401).json({ error: 'Authorization header missing' });
+    }
+    const token = authHeader.split(' ')[1];
+    if (!token) {
+        return res.status(401).json({ error: 'Token missing' });
+    }
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET || 'MOKSHU_SECRET');
+        const userId = decoded.Uid;
+        const users = await sql`SELECT * FROM users WHERE userID = ${userId}`;
+        if(users.length === 0){
+            return res.status(403).json({ error: 'malicious activity detected wrong jwt correctly signed'});
+        }
+        req.user = users[0];
+        return next(); 
+    } catch (err) {
+        console.error(err);
+        return res.status(401).json({ error: 'Invalid token' });
+    }
+}
